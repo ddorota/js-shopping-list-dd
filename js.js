5d5fab04c35a3c2cdc856fcb728bef2da5541aa5
ddorota <46536309+ddorota@users.noreply.github.com>
2019-03-03 19:47:07 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -7,24 +7,36 @@ function addNewItem(itemName) {
 
       '<div class="icons">' +
         '<div class="done">' +
-          '<input type="checkbox" >'+
+          '<input type="checkbox"/>'+
         '</div>' +
         '<div class="delete"></div>' +
       '</div>' +
       '<div class="text">' +
       itemName +
     '</div>' +
+    
     '</div>';
 
   // Append created string to end of .list div html
   // @todo make it ID not class
-  $('.list').append(elementHtml);
+  $('.list').prepend(elementHtml);
 
   // add delete event listener
   // We need to do it every time we add new div .delete
   $('.delete').click(function () {
     $(this).parent().parent().remove();
   });
+//   $('#checkbox').change(function () {
+// });
+// $(function () {
+//   $('input:checkbox').on('change', function () {
+//       if (this.checked) {
+//           $(input).css('textDecoration', 'line-through');
+//       } else {
+//           $(input).css('textDecoration', 'none');
+//       }
+//   })
+// })
 }
 
 // document is ready
@@ -33,3 +45,14 @@ $(function () {
     addNewItem($('#new-item').val());
   });
 });
+
+
+// $("input[type='checkbox']").on('change', function() {
+//   if ($(this).is(":checked")) {
+//     $(this).addClass('completed')
+//   } else {
+//     $(this).css({
+//       'text-decoration': 'none',
+//     })
+//   }
+// });
